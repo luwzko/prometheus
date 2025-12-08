e31457900960ce173c455d09a48e769db23d50ef
luwzko <luwzko@gmail.com>
2025-12-08 14:56:22 +0100

    Added reflector agent, reflects upon executed context and if an error occured.

---
+++ backend/prometheus/agents/subagents/reflector.py
@@ -0,0 +1,21 @@
+from prometheus.agents.base_agent import BaseAgent
+from prometheus.data_models.context import ExecutorContext
+from prometheus.data_models.responses import Reflection
+from prometheus.setup.config import AgentConfig
+
+import logging
+logger = logging.getLogger("prometheus.subagents.reflector")
+
+class ReflectorAgent(BaseAgent):
+    def __init__(self, agent_config: AgentConfig):
+        super().__init__(agent_config)
+
+    def execute(self, executed_context: ExecutorContext):
+        reflector_response = self._interact(executed_context.model_dump_json())
+        reflection = Reflection.model_validate(reflector_response)
+
+        if reflection is None:
+            logger.error("API error incurred.")
+            raise Exception("API error incurred.")
+
+        return reflection
